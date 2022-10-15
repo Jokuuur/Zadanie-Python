19206796097995eeefa1abc5705f1cbf09e15693
Tak <61429464+Jokuuur@users.noreply.github.com>
2022-10-15 11:32:43 +0200

    Add files via upload

---
+++ zad dom.py
@@ -0,0 +1,52 @@
+# # # #Zad.1
+# # # a = int(input("Podaj liczbę a : "))
+# # # b = int(input("Podaj liczbę b : "))
+# # # ab = a+b
+# # # if(ab%2==0):
+# # #     print("Parzysta")
+# # # else:
+# # #     print("Nie Parzysta")
+# # # #Zad.2 
+# # import math
+
+# # c = int(input("Podaj liczbę c : "))
+# # d = int(input("Podaj liczbę d : "))
+# # if((c+d)/2 > math.sqrt(d*c)):
+# #     print("Średnia arytmetyczna jest większa")
+# # else:
+# #     print("Średnia geometryczna jest większa")
+# # # #Zad.3 
+# e = int(input("Podaj liczbę e : "))
+# f = int(input("Podaj liczbę f : "))
+# g = int(input("Podaj liczbę g : "))
+# if(e==f or f==g or g==e):
+#     print("Tak")
+# else:
+#     print("Nie")
+# if(e==f):
+#     print("Te liczby to e=f")
+#     pass
+# if(f==g):
+#     print("te liczby to f=g")
+#     pass
+# if(g==e):
+#     print("Te liczby to g==e")
+#     pass
+# # #Zad.4
+# # # h = int(input("Podaj liczbę h : "))
+# # # i = int(input("Podaj liczbę i : "))
+# # # j = int(input("Podaj liczbę j : "))
+# # # k = int(input("Podaj liczbę k : "))
+# # # lista = [h,i,j,k]
+# # # print(min(lista))
+# # #Zad.5
+# # # l = float(input("Podaj najkrótszy bok : "))
+# # # m = float(input("Podaj bok o dłuższej długości niż najkrótszy ale nie większej niż największy bok : "))
+# # # n = float(input("Poproszę bok o największej długości : "))
+# # # if((l+m)>n):
+# # #     print("taki trójkąt ma szansę istnieć")
+# # # else:
+# # #     print("Taki trójkąt nie może istnieć")
+# # # if((l+m)<n):
+# # #     print("Musiałeś podać złe wartości gdyż widzisz ten komunikat bądź sprawdzasz l+m<n")
+# # #     pass
